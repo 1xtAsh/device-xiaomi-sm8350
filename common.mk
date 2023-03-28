509841edbeb60d9029e6513515015468b75632a6
Arian <arian.kulmer@web.de>
2023-03-28 18:27:56 +0200

    sm8350-common: Build graphics allocator 3.0 interface to vendor
    Some camera libraries depend on it
    
    Change-Id: I0f4638c3f383f236e813457fc88fc1c33aaa0dfc

--- common.mk
+++ common.mk
@@ -90,6 +90,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@3.0.vendor 
+
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
