98fcc2915c8d9e048ef37e4893b644d1f4ee7e13
Sugakesshaa <sugakesshaa@gmail.com>
2023-07-29 04:34:00 +0800

    sm8350-common: Separate powerhint for lahaina and yupik
    - Tuned CPU/GPU frequencies according to Snapdragon 778G
    - Copy powerhint.json from device tree according to platform name
    
    Change-Id: I44e41a0c262ec793df2869d9f80fefe82862bfe2

--- common.mk
+++ common.mk
@@ -330,8 +330,7 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
-    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # QTI
 PRODUCT_COPY_FILES += \
