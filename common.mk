c535eb762a20d35a08ef15dd7785f150021dde98
Manish Kushwaha <kushwahamanish@google.com>
2023-12-07 01:30:46 +0800

    sm8350-common: Migrate sensor HAL to AIDL interface
    Bug: 247478991
    Test: tested on-device (P23) ab/P46941077
    Test: `sensor_test -ahal list` and `sensor_test list`
    Test: `sensor_test -ahal sample -s0` and `sensor_test sample -s0`
    
    Change-Id: I40f6ab6b7780c53cd638a1f77c95f6c324dc2d15
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- common.mk
+++ common.mk
@@ -501,9 +501,9 @@ libsqlite.vendor
 
 # Sensors
 PRODUCT_PACKAGES += \
-android.frameworks.sensorservice@1.0.vendor \
-android.hardware.sensors@2.1-service.xiaomi-multihal \
-vendor.qti.hardware.display.mapper@1.1.vendor
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors-service.xiaomi-multihal \
+    vendor.qti.hardware.display.mapper@1.1.vendor
 
 PRODUCT_COPY_FILES += \
 frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
