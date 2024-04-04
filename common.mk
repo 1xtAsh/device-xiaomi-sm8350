8309ac88e3a2f6db3faf60923b56eefd2787fd82
Sugakesshaa <sugakesshaa@pixelos.net>
2024-04-05 01:20:50 +0800

    sm8350-common: Build android.hardware.graphics.common-V4-ndk
    AOSP upgraded the blob to V5 but xiaomi's libgui requires V4 version of the blob
    
    Change-Id: Ib596cde74c414bcb5ac2ea33c394ad7b83b336f0

--- common.mk
+++ common.mk
@@ -127,6 +127,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    android.hardware.graphics.common-V4-ndk \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
