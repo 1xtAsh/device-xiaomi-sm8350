29465b57ef47f4e540cb121f14f0dff00e76f5fb
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-08-18 01:44:36 +0000

    sm8350-common: Switch to AIDL QTI memtrack service
    Change-Id: I539d59e28cbac96629c6c679e00f54f69ec34ffd
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- common.mk
+++ common.mk
@@ -196,6 +196,9 @@ PRODUCT_VENDOR_PROPERTIES += \
     $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
 endif
 
+# Memtrack
+TARGET_USE_AIDL_QTI_MEMTRACK := true
+
 # Mlipay
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.mlipay@1.1.vendor \
@@ -311,6 +314,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-    
+
 # Bcr
 $(call inherit-product, vendor/bcr/bcr.mk)
