6971c0a80e45973bab790cc8e64ffe462236af4b
1xtAsh <ashraykrsingh321@gmail.com>
2024-08-24 16:30:54 +0530

    sm8350-common: Build libtensorflowlite
    dlopen failed: library "libtensorflowlite_jni_gms_client.so" not found

--- common.mk
+++ common.mk
@@ -513,6 +513,10 @@ frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_
 frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
 frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Tensor Flow Lite
+PRODUCT_PACKAGES += \
+    libtensorflowlite_jni
+
 # Thermal
 ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
