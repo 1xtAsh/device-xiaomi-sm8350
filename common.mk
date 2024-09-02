69c47cd6a1e1fb8e701b37e5be8b6fad344fb5f0
xboxfanj <jake@aospa.co>
2024-09-02 21:37:25 +0530

    [DNM] sm8350-common: copy task_profiles from system/core
    The reason Qualcomm maintains their own copy of task_profiles for
    lahaina is that API 30 does not support UClamp. However, the
    current task_profiles for API 31 and higher does support UClamp.
    Qualcomm's modifications such as I/O CGroups and audio CPUSet
    are already integrated in the CLO system/core task_profiles,
    so we can use that instead of maintaining our own.
    
    Signed-Off-by: 1xtAsh <ashraykrsingh321@gmail.com>

--- common.mk
+++ common.mk
@@ -440,7 +440,7 @@ PRODUCT_PACKAGES += \
 libssl.vendor
 
 PRODUCT_COPY_FILES += \
-$(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 PRODUCT_COPY_FILES += \
 vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
