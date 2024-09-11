e5b16a48ad540986c792a149bdd89cfc1dfa675a
1xtAsh <ashraykrsingh321@gmail.com>
2024-09-12 01:52:25 +0530

    sm8350-common: Drop some prebuilt packages

--- common.mk
+++ common.mk
@@ -122,6 +122,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.default \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
 
+# Gms
+PRODUCT_PACKAGES += \
+    RemovePackagesGms
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
