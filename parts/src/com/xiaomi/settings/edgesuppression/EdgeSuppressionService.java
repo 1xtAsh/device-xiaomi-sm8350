dad15dbba825d79ac70c9e9457999b9e0acc8afc
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-02-03 15:31:43 +0100

    sm8550-common: parts: Make sure to reset edge rejection on every panel on
     * apparently the configuration sometimes gets lost on panel reset
    
    Change-Id: I2be29370049b871cc0b68af1d2d0a66820914094

--- parts/src/com/xiaomi/settings/edgesuppression/EdgeSuppressionService.java
+++ parts/src/com/xiaomi/settings/edgesuppression/EdgeSuppressionService.java
@@ -7,9 +7,11 @@
 package com.xiaomi.settings.edgesuppression;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.IBinder;
@@ -22,11 +24,22 @@ public class EdgeSuppressionService extends Service {
 
     private EdgeSuppressionManager mEdgeSuppressionManager;
 
+    private final BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (DEBUG) Log.d(TAG, "onReceive: " + intent.getAction());
+            if (intent.getAction() == Intent.ACTION_SCREEN_ON)
+                mEdgeSuppressionManager.handleEdgeSuppressionChange();
+        }
+    };
+
     @Override
     public void onCreate() {
         if (DEBUG) Log.d(TAG, "Creating service");
         super.onCreate();
         mEdgeSuppressionManager = EdgeSuppressionManager.getInstance(getApplicationContext());
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
         getPackageManager().setComponentEnabledSetting(
                 new ComponentName(this, "com.xiaomi.settings.edgesuppression.EdgeSuppressionSettingsActivity"),
                 PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
@@ -42,6 +55,7 @@ public class EdgeSuppressionService extends Service {
     @Override
     public void onDestroy() {
         if (DEBUG) Log.d(TAG, "onDestroy");
+        unregisterReceiver(mScreenStateReceiver);
         super.onDestroy();
     }
 
