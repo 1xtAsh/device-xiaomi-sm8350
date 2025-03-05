9776f4806453b4b4e3d6d528a7d6538ec4bf61d7
New Author Name <120115258+swiitchOFF@users.noreply.github.com>
2025-03-06 01:21:20 +0530

    sm8350-common: Include Sony Dolby Atmos
    * Using dolby from: https://github.com/swiitch-OFF-Lab/hardware_dolby
    [1]: sm8350-common: Add dolby volume listener effect
    [2]: sm8350-common: audio: Bring all stream effects
    
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,7 +65,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/hidl/framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml 
 
-DEVICE_MANIFEST_FILE := \
+DEVICE_MANIFEST_FILE += \
     $(COMMON_PATH)/hidl/manifest_lahaina.xml \
     $(COMMON_PATH)/hidl/manifest_xiaomi.xml
 
