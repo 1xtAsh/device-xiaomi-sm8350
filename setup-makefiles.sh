3c8db92ce73fa7d7de44425865000e2a5a687be2
1xtAsh <ashraykrsingh321@gmail.com>
2025-04-08 23:14:43 +0530

    fixup! sm8350-common: remove OMX

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -47,7 +47,6 @@ function lib_to_package_fixup_vendor_variants() {
             vendor.qti.imsrtpservice@3.0)
             echo "$1-vendor"
             ;;
-        libOmxCore | \
             libwpa_client) ;;
         *)
             return 1
