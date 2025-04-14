93546c5ef300c5a04b1c8eea907086d1ed84aac4
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-04-14 11:13:19 +0200

    sm8550-common: Switch to python extract-utils
    Change-Id: Iaa86739e1a37c5d96ce5f58c361a76062b9396bc

---
+++ extract-files.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/qcom/common/system/telephony',
+    'vendor/qcom/common/vendor/adreno-t',
+    'vendor/qcom/common/vendor/display/5.15',
+]
+
+def lib_fixup_odm_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_odm' if partition in ('odm', 'vendor') else None
+
+def lib_fixup_sm8550_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_sm8550' if partition in ('odm', 'vendor') else None
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_vendor' if partition in ('odm', 'vendor') else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'vendor.xiaomi.hardware.fx.tunnel@1.0',
+        'vendor.xiaomi.hardware.mfidoca@1.0',
+        'vendor.xiaomi.hardware.mlipay@1.0',
+        'vendor.xiaomi.hardware.mlipay@1.1',
+        'vendor.xiaomi.hardware.mtdservice@1.0',
+        'vendor.xiaomi.hardware.mtdservice@1.1',
+        'vendor.xiaomi.hardware.mtdservice@1.2',
+        'vendor.xiaomi.hardware.mtdservice@1.3',
+        'vendor.xiaomi.hardware.tidaservice@1.0',
+        'vendor.xiaomi.hardware.tidaservice@1.1',
+        'vendor.xiaomi.hardware.tidaservice@1.2',
+    ): lib_fixup_odm_suffix,
+    (
+        'audio.primary.kalama',
+        'libsdm-color',
+        'libsdm-disp-vndapis',
+        'libsdmextension',
+    ): lib_fixup_sm8550_suffix,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'com.qualcomm.qti.imscmservice@1.0',
+        'com.qualcomm.qti.imscmservice@2.0',
+        'com.qualcomm.qti.imscmservice@2.1',
+        'com.qualcomm.qti.imscmservice@2.2',
+        'com.qualcomm.qti.uceservice@2.0',
+        'com.qualcomm.qti.uceservice@2.1',
+        'com.qualcomm.qti.uceservice@2.2',
+        'com.qualcomm.qti.uceservice@2.3',
+        'vendor.qti.data.factory@2.0',
+        'vendor.qti.data.factory@2.1',
+        'vendor.qti.data.factory@2.2',
+        'vendor.qti.data.factory@2.3',
+        'vendor.qti.data.factory@2.4',
+        'vendor.qti.data.factory@2.5',
+        'vendor.qti.data.factory@2.6',
+        'vendor.qti.data.factory@2.7',
+        'vendor.qti.data.mwqem@1.0',
+        'vendor.qti.data.slm@1.0',
+        'vendor.qti.diaghal@1.0',
+        'vendor.qti.hardware.data.cne.internal.api@1.0',
+        'vendor.qti.hardware.data.cne.internal.constants@1.0',
+        'vendor.qti.hardware.data.cne.internal.server@1.0',
+        'vendor.qti.hardware.data.cne.internal.server@1.1',
+        'vendor.qti.hardware.data.cne.internal.server@1.2',
+        'vendor.qti.hardware.data.cne.internal.server@1.3',
+        'vendor.qti.hardware.data.connection@1.0',
+        'vendor.qti.hardware.data.connection@1.1',
+        'vendor.qti.hardware.data.connectionfactory-V1-ndk',
+        'vendor.qti.hardware.data.dataactivity-V1-ndk',
+        'vendor.qti.hardware.data.dynamicdds@1.0',
+        'vendor.qti.hardware.data.dynamicdds@1.1',
+        'vendor.qti.hardware.data.flow@1.0',
+        'vendor.qti.hardware.data.flow@1.1',
+        'vendor.qti.hardware.data.iwlan@1.0',
+        'vendor.qti.hardware.data.iwlan@1.1',
+        'vendor.qti.hardware.data.ka-V1-ndk',
+        'vendor.qti.hardware.data.latency@1.0',
+        'vendor.qti.hardware.data.lce@1.0',
+        'vendor.qti.hardware.data.qmi@1.0',
+        'vendor.qti.hardware.dpmservice@1.0',
+        'vendor.qti.hardware.dpmservice@1.1',
+        'vendor.qti.hardware.embmssl@1.0',
+        'vendor.qti.hardware.embmssl@1.1',
+        'vendor.qti.hardware.limits@1.0',
+        'vendor.qti.hardware.limits@1.1',
+        'vendor.qti.hardware.limits@1.2',
+        'vendor.qti.hardware.ListenSoundModel@1.0',
+        'vendor.qti.hardware.mwqemadapter@1.0',
+        'vendor.qti.hardware.qccsyshal@1.0',
+        'vendor.qti.hardware.qccsyshal@1.1',
+        'vendor.qti.hardware.qccsyshal@1.2',
+        'vendor.qti.hardware.qccvndhal@1.0',
+        'vendor.qti.hardware.qxr-V1-ndk',
+        'vendor.qti.hardware.radio.am-V1-ndk',
+        'vendor.qti.hardware.radio.am@1.0',
+        'vendor.qti.hardware.radio.atcmdfwd-V1-ndk',
+        'vendor.qti.hardware.radio.atcmdfwd@1.0',
+        'vendor.qti.hardware.radio.ims-V9-ndk',
+        'vendor.qti.hardware.radio.ims@1.0',
+        'vendor.qti.hardware.radio.ims@1.1',
+        'vendor.qti.hardware.radio.ims@1.2',
+        'vendor.qti.hardware.radio.ims@1.3',
+        'vendor.qti.hardware.radio.ims@1.4',
+        'vendor.qti.hardware.radio.ims@1.5',
+        'vendor.qti.hardware.radio.ims@1.6',
+        'vendor.qti.hardware.radio.ims@1.7',
+        'vendor.qti.hardware.radio.ims@1.8',
+        'vendor.qti.hardware.radio.internal.deviceinfo-V1-ndk',
+        'vendor.qti.hardware.radio.internal.deviceinfo@1.0',
+        'vendor.qti.hardware.radio.lpa@1.0',
+        'vendor.qti.hardware.radio.lpa@1.1',
+        'vendor.qti.hardware.radio.lpa@1.2',
+        'vendor.qti.hardware.radio.lpa@1.3',
+        'vendor.qti.hardware.radio.qcrilhook-V1-ndk',
+        'vendor.qti.hardware.radio.qcrilhook@1.0',
+        'vendor.qti.hardware.radio.qtiradio-V9-ndk',
+        'vendor.qti.hardware.radio.qtiradio@1.0',
+        'vendor.qti.hardware.radio.qtiradio@2.0',
+        'vendor.qti.hardware.radio.qtiradio@2.1',
+        'vendor.qti.hardware.radio.qtiradio@2.2',
+        'vendor.qti.hardware.radio.qtiradio@2.3',
+        'vendor.qti.hardware.radio.qtiradio@2.4',
+        'vendor.qti.hardware.radio.qtiradio@2.5',
+        'vendor.qti.hardware.radio.qtiradio@2.6',
+        'vendor.qti.hardware.radio.qtiradioconfig-V3-ndk',
+        'vendor.qti.hardware.radio.uim@1.0',
+        'vendor.qti.hardware.radio.uim@1.1',
+        'vendor.qti.hardware.radio.uim@1.2',
+        'vendor.qti.hardware.radio.uim_remote_client@1.0',
+        'vendor.qti.hardware.radio.uim_remote_client@1.1',
+        'vendor.qti.hardware.radio.uim_remote_client@1.2',
+        'vendor.qti.hardware.radio.uim_remote_server@1.0',
+        'vendor.qti.hardware.sigma_miracast@1.0',
+        'vendor.qti.hardware.slmadapter@1.0',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.ims.callcapability@1.0',
+        'vendor.qti.ims.callinfo@1.0',
+        'vendor.qti.ims.configservice@1.0',
+        'vendor.qti.ims.configservice@1.1',
+        'vendor.qti.ims.connection@1.0',
+        'vendor.qti.ims.factory@1.0',
+        'vendor.qti.ims.factory@1.1',
+        'vendor.qti.ims.factory@2.0',
+        'vendor.qti.ims.factory@2.1',
+        'vendor.qti.ims.factory@2.2',
+        'vendor.qti.ims.rcsconfig@1.0',
+        'vendor.qti.ims.rcsconfig@1.1',
+        'vendor.qti.ims.rcsconfig@2.0',
+        'vendor.qti.ims.rcsconfig@2.1',
+        'vendor.qti.ims.rcssip@1.0',
+        'vendor.qti.ims.rcssip@1.1',
+        'vendor.qti.ims.rcssip@1.2',
+        'vendor.qti.ims.rcsuce@1.0',
+        'vendor.qti.ims.rcsuce@1.1',
+        'vendor.qti.ims.rcsuce@1.2',
+        'vendor.qti.imsrtpservice@3.0',
+        'vendor.qti.imsrtpservice@3.1',
+        'vendor.qti.latency@2.0',
+        'vendor.qti.latency@2.1',
+        'vendor.qti.latency@2.2',
+        'vendor.qti.latency@2.3',
+        'vendor.xiaomi.hardware.displayfeature@1.0',
+        'vendor.xiaomi.hardware.fingerprintextension@1.0',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libcamxcommonutils',
+        'libwpa_client',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'odm/lib64/hw/displayfeature.default.so': blob_fixup()
+        .replace_needed(
+            'libstagefright_foundation.so',
+            'libstagefright_foundation-v33.so',
+        ),
+    'odm/lib64/libmt@1.3.so': blob_fixup()
+        .replace_needed(
+            'libcrypto.so',
+            'libcrypto-v33.so',
+        ),
+    (
+        'vendor/bin/hw/android.hardware.security.keymint-service-qti',
+        'vendor/lib64/libqtikeymint.so',
+    ): blob_fixup()
+        .add_needed('android.hardware.security.rkp-V3-ndk.so'),
+    (
+        'vendor/bin/hw/dolbycodec2',
+        'vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service',
+        'vendor/bin/hw/vendor.dolby.media.c2@1.0-service', 
+        'vendor/lib64/hw/audio.primary.kalama.so',
+    ): blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so'),
+    'vendor/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy': blob_fixup()
+        .add_line_if_missing('setsockopt: 1'),
+    'vendor/etc/seccomp_policy/qwesd@2.0.policy': blob_fixup()
+        .add_line_if_missing('pipe2: 1'),
+    'vendor/lib64/c2.dolby.client.so': blob_fixup()
+        .add_needed('libcodec2_hidl_shim.so'),
+    'vendor/lib64/libsnpe_config.so': blob_fixup()
+        .add_needed('liblog.so'),
+    'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
+        .add_needed('libhidlbase_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'sm8550-common',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
