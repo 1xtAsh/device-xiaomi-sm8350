dea655104be34634b1cc0b2b7d4a6417b3d50f1a
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    sm8350-common: Move Lineage Health HAL to select()
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- common.mk
+++ common.mk
@@ -257,6 +257,8 @@ PRODUCT_ENABLE_UFFD_GC := false
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/init.qti.media.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.media.sh \
