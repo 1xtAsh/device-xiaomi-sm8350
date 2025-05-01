d8ce4b7d06736332d6f6401465605046a6d6def3
1xtAsh <ashraykrsingh321@gmail.com>
2025-05-01 12:11:24 +0530

    sm8350-common: Build libutilscallstack
    * E vndksupport: Could not load /vendor/lib64/hw/camera.qcom.so from default namespace: dlopen failed: library "libutilscallstack.so" not found: needed by /vendor/lib64/libofflinelog.so in namespace (default).

--- common.mk
+++ common.mk
@@ -79,7 +79,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
     libcamera2ndk_vendor \
     libion.vendor \
-    libpng.vendor
+    libpng.vendor \
+    libutilscallstack.vendor \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt
