bbc880b58e0695f62afe86f7c199f87b121798a3
1xtAsh <ashraykrsingh321@gmail.com>
2025-11-09 08:29:25 +0530

    sm8350-common: Drop lineage health
    Lineage health is not available on YAAP.

--- common.mk
+++ common.mk
@@ -258,12 +258,6 @@ PRODUCT_COPY_FILES += \
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
-# Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/init.qti.media.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.media.sh \
