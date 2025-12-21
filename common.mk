c9cf37002592534c46ea2230bf79aedf6aa24c00
1xtAsh <ashraykrsingh321@gmail.com>
2025-12-21 11:13:29 +0000

    sm8350-common: Update some soong config variables to bool type

--- common.mk
+++ common.mk
@@ -146,7 +146,7 @@ PRODUCT_PACKAGES += \
 # DeviceAsWebcam
 PRODUCT_PACKAGES += \
 		DeviceAsWebcam
-		
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
@@ -283,7 +283,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/yupik/media_codecs_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_yupik_v1.xml \
     $(LOCAL_PATH)/media/yupik/media_profiles_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_yupik_v1.xml
 
-$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+$(call soong_config_set_bool,stagefright,target_disable_thumbnail_block_model,true)
 
 # Native libraries whitelist
 PRODUCT_COPY_FILES += \
